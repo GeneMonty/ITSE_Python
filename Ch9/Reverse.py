a7ca2299b3d1480e954521de7c07d310b43326e8
GeneMonty <36083539+GeneMonty@users.noreply.github.com>
2019-11-13 17:13:31 -0600

    complete

--- Ch9/Reverse.py
+++ Ch9/Reverse.py
@@ -6,12 +6,19 @@ Output: Original contents of array and the reversed contents of the array.
 
 
 # Write reverseArray function here.
+def rev(x):
+        y=[]
+        while x:
+                 y.append(x.pop(len(x)-1))
+        return y
 
 
-numbers = [9, 8, 7, 6, 5]
+x = [9, 8, 7, 6, 5]
 
 # Print contents of array.
-
+print("Original contents of array:",x)
 # Call reverseArray function here.
-
+y=rev(x)
 # Print contents of reversed array.
+print("Reversed contents of array:",y)
+
